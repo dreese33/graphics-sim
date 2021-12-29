8b982183240ac08962d3e0af460b394742145887
dreese33 <dylanjacobreese@gmail.com>
2021-12-28 22:49:51 -0500

    feat: add random polynomial animation

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # graphics-sim
+
 Playing around with a variety of Python libraries for cool physics and graphics simulations.
+
+## Virtual Environment Setup:
+
+To setup a python virtual environment, you can run the following command:
+`python -m venv /path/to/env`
+
+Once complete, the environment can be activated via:
+`source /path/to/env/bin/activate`
+
+To install the dependencies, run the following from the cloned repository:
+`pip install -r requirements.txt`
---
+++ polynomials.py
@@ -0,0 +1,48 @@
+from vpython import *
+import numpy as np
+
+
+def create_origin_line():
+  """
+  Line with first point, beginning at the origin of the graph
+  """
+  moving_line = curve()
+  moving_line.append(
+    vector(0, 0, 0),
+  )
+
+  return moving_line
+
+
+def draw_parabola_animation(a, b, c, totalPts = 5):
+  """
+  Equation for a parabola:
+  y = ax^2 + bx + c
+  """
+  moving_line = create_origin_line()
+
+  for x in range(0, totalPts):
+    rate(5)
+    y = a*x*x + b*x + c
+    moving_line.append(
+      vector(x, y, 0)
+    )
+
+
+# def draw_mandelbrot_animation(C, totalPts = 10):
+#   """
+#   Mandelbrot set can be generated via:
+#   Z_{n+1} = Z_{n}^2 + C
+#   """
+#   z_n = 0
+#   moving_line = create_origin_line()
+
+#   for t in range(0, totalPts):
+#     rate(5)
+#     z_n = z_n*z_n + C
+#     moving_line.append(
+#       vector(t, z_n, 0)
+#     )
+
+draw_parabola_animation(1, -2, 0, 20)
+# draw_mandelbrot_animation(2)
